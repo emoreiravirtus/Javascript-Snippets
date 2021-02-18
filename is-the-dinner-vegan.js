a69e6d1063b810935a1af7ca222c48ac6d9354f5
Elias Moreira <56691312+emoreiravirtus@users.noreply.github.com>
2021-02-17 22:42:56 -0300

    Create is-the-dinner-vegan.js

---
+++ is-the-dinner-vegan.js
@@ -0,0 +1,8 @@
+const isTheDinnerVegan = arr => arr.every(element => element.source === 'plant')
+
+// Feel free to comment out the code below to test your function
+
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+
+console.log(isTheDinnerVegan(dinner))
+// Should print false
