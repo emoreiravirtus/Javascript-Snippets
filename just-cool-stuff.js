d5f6623965f123a959757ace6f0006ef08a5e89c
Elias Moreira <56691312+emoreiravirtus@users.noreply.github.com>
2021-02-17 22:40:23 -0300

    Create just-cool-stuff.js

---
+++ just-cool-stuff.js
@@ -0,0 +1,9 @@
+const justCoolStuff = (arr1, arr2) => arr1.filter(element => arr2.includes(element))
+
+// Feel free to uncomment the code below to test your function
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
