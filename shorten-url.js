d6eaf7a981e2d46f55c60ac7f0a5da57a64972b9
Elias Moreira <56691312+emoreiravirtus@users.noreply.github.com>
2021-03-05 22:23:08 -0300

    Create shorten-url.js

---
+++ shorten-url.js
@@ -0,0 +1,41 @@
+// Information to reach API
+const apiKey = '26154e9d5eab4cc4bb35bffb6fe668c2';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const xhr = new XMLHttpRequest();
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+
+  xhr.responseType = 'json'
+
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === XMLHttpRequest.DONE){
+      renderResponse(xhr.response);
+    }
+  }
+
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.setRequestHeader('apikey', apiKey)
+  xhr.send(data);
+
+}
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
